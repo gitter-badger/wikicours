3cc2c6d11550ffafd75c70fe468c1534e7ac39d1
Thomas KUNTZ <thomaskuntzz@protonmail.com>
2018-04-12 14:28:37 +0200

    Add substance and styling to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,9 @@ import Link from 'gatsby-link'
 import get from 'lodash/get'
 import Helmet from 'react-helmet'
 
-class BlogIndex extends React.Component {
+import styles from './index.module.css'
+
+class HomePage extends React.Component {
   render() {
     const siteTitle = get(this, 'props.data.site.siteMetadata.title')
     const posts = get(this, 'props.data.allMarkdownRemark.edges')
@@ -11,13 +13,89 @@ class BlogIndex extends React.Component {
     return (
       <div>
         <Helmet title={siteTitle} />
-        <code>this is the home page</code>
+
+        <p className="about">
+          Wikicours est un répertoire de cours en ligne de qualité, 📚✨ constamment amélioré par une communauté de professeurs et d'utilisateurs. 📝 Le tout gratuit et libre de droit. 💸🆓
+        </p>
+
+        <section className={styles.schoolAndGrade}>
+          <div>
+            <h4>Collège</h4>
+
+            <div>
+              {/* <Link to=''>6<sup>ème</sup></Link> */}
+              <Link to=''>6e</Link>
+              <Link to=''>5e</Link>
+              <Link to=''>4e</Link>
+              <Link to=''>3e</Link>
+            </div>
+          </div>
+
+          <div>
+            <h4>Lycée</h4>
+
+            <div>
+              {/* <Link to=''>2<sup>nde</sup></Link> */}
+              <Link to=''>2nde</Link>
+              <Link to=''>1ère</Link>
+              <Link to=''>Terminale</Link>
+            </div>
+          </div>
+        </section>
+
+        <section>
+          <h2>
+            Contribuer
+          </h2>
+          
+          <p>
+            Tout le monde peut améliorer Wikicours.
+          </p>
+          
+          <p>
+            Users can already remix any public app on Glitch, and we want to give project owners control over their app's look,
+            so we do not force buttons into any apps. What we do want, though, is for our community to <em>encourage</em> 
+            others to remix and learn more about their apps, so we built a widget that you can include in your projects to generate 
+            our neat little button wherever you like.
+          </p>
+          
+          <p>
+            Here's what it looks like: 
+          </p>
+        </section>
+        
+        <section>
+          <h2>
+            Add the button!
+          </h2>
+          
+          <p>
+            Users can already remix any public app on Glitch, and we want to give project owners control over their app's look,
+            so we do not force buttons into any apps. What we do want, though, is for our community to <em>encourage</em> 
+            others to remix and learn more about their apps, so we built a widget that you can include in your projects to generate 
+            our neat little button wherever you like.
+          </p>
+          
+          <p>
+            Here's what it looks like: 
+          </p>
+        </section>
+        
+        <section>
+          <h2>
+            Happy Glitching! 
+          </h2>
+          
+          <p>
+            We want to know about all the cool stuff you're making, remixed or not–let us know it by tweeting <a href="http://twitter.com/glitch">@glitch</a>! 
+          </p>
+        </section>
       </div>
     )
   }
 }
 
-export default BlogIndex
+export default HomePage
 
 export const pageQuery = graphql`
   query IndexQuery {
